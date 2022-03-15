1fd06aae13a3fb7a125391acde9b1261426c115b
Lucas França <lucasvpessoafranca@gmail.com>
2022-03-15 08:52:43 -0300

    edicao concluida

--- controller/ToughtController.js
+++ controller/ToughtController.js
@@ -70,4 +70,15 @@ module.exports = class ToughtController {
 
     res.render('toughts/edit', {tought})
   }
+  static async updateToughtSave(req,res) {
+    const id = req.body.id
+    const tought = {
+      title:req.body.title
+    }
+    await Tought.update(tought, {where: {id:id}})
+    req.flash('message', 'Pensamento atualizado com sucesso!')
+    req.session.save(()=> {
+      res.redirect('/toughts/dashboard')
+    })
+  } 
 }
