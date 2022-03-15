01c487e78e5a0b011c9c9842b509779129cbf5f5
Lucas França <lucasvpessoafranca@gmail.com>
2022-03-15 00:13:51 -0300

    enviando pensamento

--- controller/ToughtController.js
+++ controller/ToughtController.js
@@ -6,4 +6,22 @@ module.exports = class ToughtController {
   static async showToughts(req,res) {
     res.render('toughts/home')
   }
+  static async dashboard(req,res) {
+    res.render('toughts/dashboard')
+  }
+  static createTought (req,res) {
+    res.render('toughts/create')
+  }  
+  static async createToughtSave(req,res) {
+    const tought = {
+      title: req.body.title,
+      UserId: req.session.userid
+    }
+    await Tought.create(tought)
+    req.flash('message', 'Pensamento criado com sucesso')
+    req.session.save(()=> {
+      res.redirect('/toughts/dashboard')
+    })
+  }
+
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const ToughtController = require('../controller/ToughtController')
-router.get('/', ToughtController.showToughts)
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/add', checkAuth, ToughtController.createTought)
+router.post('/add', checkAuth, ToughtController.createToughtSave)
+router.get('/dashboard', checkAuth, ToughtController.dashboard)
+router.get('/',ToughtController.showToughts)
 
 module.exports = router
